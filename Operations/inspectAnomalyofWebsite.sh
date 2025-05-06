41cbc441ea67ea64d33e0aa13d715aeb57928469
Gabriel-Antonio-Sephiroth <131593045+Gabriel-Antonio-Sephiroth@users.noreply.github.com>
2025-05-06 16:12:09 +0800

    更新 inspectAnomalyofWebsite.sh

--- Operations/inspectAnomalyofWebsite.sh
+++ Operations/inspectAnomalyofWebsite.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+URL_LIST="www.baidu.com [url=http://www.ctnrs.com]www.ctnrs.com[/url]"
+for URL in $URL_LIST; do
+    FAIL_COUNT=0
+    for ((i=1;i&lt;=3;i++)); do
+        HTTP_CODE=$(curl -o /dev/null --connect-timeout 3 -s -w "%{http_code}" $URL)
+        if [ $HTTP_CODE -eq 200 ]; then
+            echo "$URL OK"
+            break
+        else
+            echo "$URL retry $FAIL_COUNT"
+            let FAIL_COUNT++
+        fi
+    done
+    if [ $FAIL_COUNT -eq 3 ]; then
+        echo "Warning: $URL Access failure!"
+    fi
+done
